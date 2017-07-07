772c12f97adf8854f2bd44516c5f90304ed36bbf
yangjiangfeng <yjfdl123@163.com>
2017-07-07 18:14:04 +0800

    data hongzhankai

--- data.cpp
+++ data.cpp
@@ -1,4 +1,17 @@
-truct event_list{
+struct event_list{
     struct event * tqh_first;
     struct event ** tqh_last;
+};
+
+struct ev_next{								
+	struct event *tqe_next;	/* next element */			
+	struct event **tqe_prev;	/* address of previous next element */	
+};
+struct ev_active_next{								
+	struct event *tqe_next;	/* next element */			
+	struct event **tqe_prev;	/* address of previous next element */	
+};
+struct ev_signal_next{								
+	struct event *tqe_next;	/* next element */			
+	struct event **tqe_prev;	/* address of previous next element */	
 };
